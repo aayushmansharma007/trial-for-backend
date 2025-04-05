5ad93230fa5c971819ef0e83cf3204c77c75aff0
aayushmansharma007 <aayushman2006lanos@gmail.com>
2025-04-05 22:18:46 +0530

    initial commit

--- src/main/java/com/example/practicekroonga/thikhai/controller/OrderController.java
+++ src/main/java/com/example/practicekroonga/thikhai/controller/OrderController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/orders")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://trial-bzhbwagdk-aayushmansharma007s-projects.vercel.app")
 public class OrderController {
     @Autowired
     private OrderService orderService;
