6e6aac4592407088ad895e9ca54520701c4d8123
aayushmansharma007 <aayushman2006lanos@gmail.com>
2025-04-08 22:29:42 +0530

    initial commit

--- src/main/java/com/example/practicekroonga/thikhai/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.practicekroonga.thikhai.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.practicekroonga.thikhai.Entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- src/main/java/com/example/practicekroonga/thikhai/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-// ProductRepository.java
-package com.example.practicekroonga.thikhai.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.practicekroonga.thikhai.Entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
